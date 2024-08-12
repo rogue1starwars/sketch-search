07af3f4233339821268436f683b5e8507df93191
Tetsuka <rogue1starwars728@yahoo.co.jp>
2024-08-12 20:52:47 +0900

    initial commit

---
+++ renderer.js
@@ -0,0 +1,30 @@
+const button = document.getElementById("fileSelector");
+button.addEventListener("click", async () => {
+  const [keypoints, filePath] = await window.versions.openFileDialog();
+  const image = document.getElementById("image");
+  console.log(keypoints);
+  const keypoint = keypoints[0];
+  image.src = `file://${filePath}`;
+  for (let i = 0; i < keypoint.length; i += 2) {
+    const circle = document.createElement("div");
+    circle.className = "circle";
+    circle.style.top = `${keypoint[i + 1]}px`;
+    circle.style.left = `${keypoint[i]}px`;
+    circle.style.width = "10px";
+    circle.style.height = "10px";
+    circle.style.position = "absolute";
+    circle.style.border = "1px solid red";
+    circle.style.borderRadius = "50%";
+    image.parentElement.appendChild(circle);
+  }
+  const circle = document.createElement("div");
+  circle.className = "circle";
+  circle.style.top = `0px`;
+  circle.style.left = `0px`;
+  circle.style.width = "10px";
+  circle.style.height = "10px";
+  circle.style.position = "absolute";
+  circle.style.border = "1px solid red";
+  circle.style.borderRadius = "50%";
+  image.parentElement.appendChild(circle);
+});
